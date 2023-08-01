41d82676f2d7ad26c18445b0b144efe71a912c07
stefanlazarevic <stefanlaza97@gmail.com>
2023-08-01 14:17:30 -0700

    add phase 3

--- greenhouse/src/context/ClimateContext.js
+++ greenhouse/src/context/ClimateContext.js
@@ -1,2 +1,25 @@
-// Temperature has a default value of 50 degrees
-// Humidity has a default value of 40%
+import { createContext, useContext, useState } from "react";
+
+const ClimateContext = createContext();
+
+export const ClimateProvider  = ({children}) => { 
+    const [temperature, setTemperature] = useState(50); // Temperature has a default value of 50 degrees
+    const [humidity, setHumidity] = useState(40); // Humidity has a default value of 40%
+    console.log(temperature);
+    
+    return(
+        <ClimateContext.Provider
+            value={{
+                temperature,
+                setTemperature,
+                humidity,
+                setHumidity
+            }}>
+            {children}
+        </ClimateContext.Provider>
+    );
+}
+
+export function useClimate() { 
+    return useContext(ClimateContext);
+}
